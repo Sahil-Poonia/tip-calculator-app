dae57657bcb5d24582cdd1f8611102468f977916
Sahil Poonia <poonia.sp2002@gmail.com>
2022-06-25 17:43:05 +0530

    feat(js): add complete functionality

--- script.js
+++ script.js
@@ -1,14 +1,60 @@
+const billInput = document.getElementById("input-bill");
 const radioInputs = document.getElementsByName("tip-percentage");
-let tipPrecentage;
+const peopleInput = document.getElementById("total-people");
+const timAmtOutput = document.getElementById("tip-amt");
+const totalPeopleOutput = document.getElementById("total-tip");
+const resetBtn = document.getElementById("reset-btn");
+
+let tipPercentage = 0,
+  billAmount = parseInt(billInput.value),
+  totalPeople = parseInt(peopleInput.value);
+
+const calcTip = (bill, tip, people) => {
+  const tipAmtPerPerson = (bill * (tip / 100)) / people;
+  const totalPerPerson = tipAmtPerPerson + bill / people;
+  return [tipAmtPerPerson, totalPerPerson];
+};
+
+const updateOutput = (tipAmt, total) => {
+  timAmtOutput.textContent = tipAmt.toFixed(2);
+  totalPeopleOutput.textContent = total.toFixed(2);
+  resetBtn.disabled = false;
+};
+
+billInput.addEventListener("input", (event) => {
+  billAmount = parseInt(event.target.value);
+  updateOutput(...calcTip(billAmount, tipPercentage, totalPeople));
+});
 
 radioInputs.forEach((element) => {
   element.addEventListener("input", (event) => {
-    tipPrecentage = parseInt(event.target.value);
+    tipPercentage = parseInt(event.target.value);
     event.target.parentElement.classList.add("bg-strong-cyan");
     for (const radioInput of radioInputs) {
       if (!radioInput.checked || event.target.type === "number") {
         radioInput.parentElement.classList.remove("bg-strong-cyan");
       }
     }
+    updateOutput(...calcTip(billAmount, tipPercentage, totalPeople));
   });
+});
+
+peopleInput.addEventListener("input", (event) => {
+  totalPeople = parseInt(event.target.value);
+  updateOutput(...calcTip(billAmount, tipPercentage, totalPeople));
+});
+
+resetBtn.addEventListener("click", (event) => {
+  billInput.value = 0;
+  for (const radioInput of radioInputs) {
+    radioInput.checked = false;
+    radioInput.parentElement.classList.remove("bg-strong-cyan");
+  }
+  peopleInput.value = 1;
+  timAmtOutput.textContent = "0.00";
+  totalPeopleOutput.textContent = "0.00";
+  resetBtn.disabled = true;
+  tipPercentage = 0;
+  billAmount = parseInt(billInput.value);
+  totalPeople = parseInt(peopleInput.value);
 });
