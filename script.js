c6c38c385302280c6167ce12fc3a535f205c1b90
Sahil Poonia <poonia.sp2002@gmail.com>
2022-06-26 06:45:19 +0530

    style: add app responsiveness and code refactoring

--- script.js
+++ script.js
@@ -1,60 +1,73 @@
-const billInput = document.getElementById("input-bill");
-const radioInputs = document.getElementsByName("tip-percentage");
-const peopleInput = document.getElementById("total-people");
-const timAmtOutput = document.getElementById("tip-amt");
-const totalPeopleOutput = document.getElementById("total-tip");
-const resetBtn = document.getElementById("reset-btn");
+// Cache all the input and output elements
+const billElement = document.getElementById("input-bill"),
+  tipElements = document.getElementsByName("tip-percentage"),
+  peopleElement = document.getElementById("number-of-people"),
+  tipOutputElement = document.getElementById("tip-amt-per-person"),
+  totalOutputElement = document.getElementById("total-tip-per-person"),
+  resetBtn = document.getElementById("reset-btn");
 
-let tipPercentage = 0,
-  billAmount = parseInt(billInput.value),
-  totalPeople = parseInt(peopleInput.value);
+// necessary global variables
+let bill, tip, people;
 
-const calcTip = (bill, tip, people) => {
-  const tipAmtPerPerson = (bill * (tip / 100)) / people;
-  const totalPerPerson = tipAmtPerPerson + bill / people;
-  return [tipAmtPerPerson, totalPerPerson];
+const reset = () => {
+  billElement.value = "";
+
+  // reseting all radio buttons with their styles
+  tipElements.forEach((element) => {
+    if (element.type === "number") element.value = "";
+    element.parentElement.classList.remove("bg-strong-cyan");
+    element.checked = false;
+  });
+
+  peopleElement.value = "";
+  tipOutputElement.textContent = "0.00";
+  totalOutputElement.textContent = "0.00";
+  resetBtn.disabled = true;
+
+  bill = 0;
+  tip = 0;
+  people = 1;
 };
 
-const updateOutput = (tipAmt, total) => {
-  timAmtOutput.textContent = tipAmt.toFixed(2);
-  totalPeopleOutput.textContent = total.toFixed(2);
+const calcTip = (billAmount, tipInPercent, people) => {
+  const tipAmountPerPerson = (billAmount * (tipInPercent / 100)) / people;
+  const totalPerPerson = billAmount / people + tipAmountPerPerson;
+  return [tipAmountPerPerson, totalPerPerson];
+};
+
+const updateOutput = (tipAmountPerPerson, totalPerPerson) => {
+  tipOutputElement.textContent = tipAmountPerPerson.toFixed(2);
+  totalOutputElement.textContent = totalPerPerson.toFixed(2);
   resetBtn.disabled = false;
 };
 
-billInput.addEventListener("input", (event) => {
-  billAmount = parseInt(event.target.value);
-  updateOutput(...calcTip(billAmount, tipPercentage, totalPeople));
+// Initialize the app
+reset();
+
+billElement.addEventListener("input", (event) => {
+  bill = parseFloat(event.target.value);
+  updateOutput(...calcTip(bill, tip, people));
 });
 
-radioInputs.forEach((element) => {
+tipElements.forEach((element) => {
   element.addEventListener("input", (event) => {
-    tipPercentage = parseInt(event.target.value);
+    tip = parseFloat(event.target.value);
     event.target.parentElement.classList.add("bg-strong-cyan");
-    for (const radioInput of radioInputs) {
-      if (!radioInput.checked || event.target.type === "number") {
-        radioInput.parentElement.classList.remove("bg-strong-cyan");
-      }
+
+    for (const tipElement of tipElements) {
+      if (!tipElement.checked || event.target.type === "number")
+        tipElement.parentElement.classList.remove("bg-strong-cyan");
     }
-    updateOutput(...calcTip(billAmount, tipPercentage, totalPeople));
+
+    updateOutput(...calcTip(bill, tip, people));
   });
 });
 
-peopleInput.addEventListener("input", (event) => {
-  totalPeople = parseInt(event.target.value);
-  updateOutput(...calcTip(billAmount, tipPercentage, totalPeople));
+peopleElement.addEventListener("input", (event) => {
+  people = parseFloat(event.target.value);
+  updateOutput(...calcTip(bill, tip, people));
 });
 
 resetBtn.addEventListener("click", (event) => {
-  billInput.value = 0;
-  for (const radioInput of radioInputs) {
-    radioInput.checked = false;
-    radioInput.parentElement.classList.remove("bg-strong-cyan");
-  }
-  peopleInput.value = 1;
-  timAmtOutput.textContent = "0.00";
-  totalPeopleOutput.textContent = "0.00";
-  resetBtn.disabled = true;
-  tipPercentage = 0;
-  billAmount = parseInt(billInput.value);
-  totalPeople = parseInt(peopleInput.value);
+  reset();
 });
